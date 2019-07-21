064fe0191693379a50c3790e4d7fd5328256851e
Andiappan <andiappan.r@cogniozant.com>
2019-07-21 17:59:49 +0530

    Draft version : Initial commit

---
+++ html2canvas/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace html2canvas.Controllers
+{
+
+    public class HomeController : Controller
+    {     
+
+        public ActionResult DownloadItem(string url)
+        {
+            WebClient myWebClient = new WebClient();
+            byte[] myDataBuffer = myWebClient.DownloadData(url);
+            return File(myDataBuffer, "image/jpeg", "ImageName");
+        }
+
+      
+    }
+}
